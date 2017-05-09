84502c442ac20067b7e2a89392ef4f281f001b92
Allan Chau <allan.chau@icloud.com>
2017-05-10 08:33:29 +0930

    Initial commit

---
+++ src/browser.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'jquery': true,
+    },
+    'extends': 'idearium/common',
+    'parserOptions': {
+        'ecmaVersion': 5,
+        'sourceType': 'script',
+    },
+};
