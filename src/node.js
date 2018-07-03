5d073e3bc59ad50c103c6d8c3dc406b9bc84bdd2
Scott Mebberson <scott@scottmebberson.com>
2018-07-03 10:20:48 +0930

    Updated to use the experimentalObjectRestSpread emcaFeature.

--- src/node.js
+++ src/node.js
@@ -8,7 +8,10 @@ module.exports = {
     },
     'extends': '@idearium/eslint-config/src/common',
     'parserOptions': {
-        'ecmaVersion': 2018,
+        'ecmaFeatures': {
+            'experimentalObjectRestSpread': true,
+        },
+        'ecmaVersion': 6,
         'sourceType': 'script',
     },
     'rules': {
@@ -19,7 +22,6 @@ module.exports = {
         'no-process-exit': 'error',
         'no-restricted-modules': 'off',
         'no-sync': 'error',
-
         'arrow-body-style': 'off',
         'arrow-parens': 'error',
         'arrow-spacing': 'error',
