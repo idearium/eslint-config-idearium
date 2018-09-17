aa155219a246f0589d39c92123121bb5afa0a90a
Scott Mebberson <scott@scottmebberson.com>
2018-09-17 11:39:34 +0930

    Further rule customisations for Linz, and now using the babel-eslint parser.

--- src/react.js
+++ src/react.js
@@ -9,13 +9,16 @@ module.exports = {
         '@idearium/eslint-config/src/common',
         'plugin:react/recommended'
     ],
+    'parser': 'babel-eslint',
     'parserOptions': {
         'ecmaVersion': 6,
         'sourceType': 'module',
     },
     rules: {
         'class-methods-use-this': 'off',
+        'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
         'react/jsx-closing-bracket-location': 'error',
+        'require-jsdoc': 'off',
         'prefer-object-spread': 'off',
     }
 };
