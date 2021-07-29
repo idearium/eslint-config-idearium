245342ab3b7b7a1f449a44ce914b66a2681c8586
allanchau <allan.chau@icloud.com>
2021-07-29 13:34:06 +0930

    migrate to eslint 7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 'use strict';
 
-module.exports = require('./node');
+const config = {
+    env: {
+        'browser': true,
+        'commonjs': true,
+        'es6': true,
+        'jquery': true,
+        'node': true,
+        'shared-node-browser': true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:node/recommended',
+        'plugin:@next/next/recommended',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        'no-await-in-loop': 'error',
+        'no-console': 'error',
+        'no-loss-of-precision': 'error',
+        'no-template-curly-in-string': 'error',
+        'no-unreachable-loop': 'error',
+        'no-unsafe-optional-chaining': 'error',
+        'no-useless-backreference': 'error',
+        'require-atomic-updates': 'error',
+        'no-label-var': 'error',
+        'no-shadow': 'error',
+        'no-undef-init': 'error',
+        'no-undefined': 'error',
+        'no-use-before-define': 'error',
+        'no-duplicate-imports': ['error', { includeExports: true }],
+        'no-useless-computed-key': 'error',
+        'no-useless-constructor': 'error',
+        'no-useless-rename': 'error',
+        'prefer-const': 'error',
+        'prefer-destructuring': 'error',
+        'prefer-rest-params': 'error',
+    },
+};
+
+module.exports = config;
