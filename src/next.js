a9b25854f2b8068f404aec1418d05c0c53eeac40
Allan Chau <allan.chau@icloud.com>
2025-04-09 08:15:32 +0930

    fix baseDirectory

--- src/next.js
+++ src/next.js
@@ -3,7 +3,7 @@ import common from '@idearium/eslint-config/src/common.js';
 import { FlatCompat } from '@eslint/eslintrc';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
-const compat = new FlatCompat();
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
 
 export default defineConfig([
     common,
